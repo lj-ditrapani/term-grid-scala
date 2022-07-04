f54a861184460cb5fc02b3433fc10cc070c39644
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-04 18:11:29 -0400

    Rearrange code to be more readable

--- src/main/scala/info/ditrapani/termgrid/main.scala
+++ src/main/scala/info/ditrapani/termgrid/main.scala
@@ -5,11 +5,6 @@ import org.jline.keymap.{BindingReader, KeyMap}
 import org.jline.keymap.KeyMap.ctrl
 
 @main def main: Unit =
-  println("Term-grid!\n")
-  val terminal = TerminalBuilder.terminal().nn
-  terminal.enterRawMode()
-  val reader = terminal.reader().nn
-  val keyReader = new BindingReader(terminal.reader())
 
   enum Action:
     case QUIT
@@ -18,9 +13,12 @@ import org.jline.keymap.KeyMap.ctrl
   val keyMap = new KeyMap[Action]()
   keyMap.bind(Action.QUIT, "Q", "q", ctrl('c'))
   keyMap.bind(Action.SPACE, " ")
-  val action = keyReader.readBinding(keyMap, null, true)
-  println(action)
+  val terminal = TerminalBuilder.terminal().nn
+  terminal.enterRawMode()
+  val reader = terminal.reader().nn
+  val keyReader = new BindingReader(terminal.reader())
 
-  println(reader.peek(500))
-  val c = reader.read()
-  println(c)
+  println("Term-grid!\n")
+  println(reader.read())
+  val action = keyReader.readBinding(keyMap)
+  println(action)
