4382a1da570156bcc9f8d5f92c7f20e0f374f30f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-04 22:02:21 -0400

    Start working on the library api

--- src/main/scala/info/ditrapani/termgrid/TermGrid.scala
+++ src/main/scala/info/ditrapani/termgrid/TermGrid.scala
@@ -1,9 +1,21 @@
 package info.ditrapani.termgrid
 
+import org.jline.terminal.Terminal
+import zio.{Task, Queue, UIO}
+
 trait ITermGrid:
-  def clear(): Unit
-  def draw(): Unit
-  def onInput(handler: String => Unit): Unit
-  def reset(): Unit
-  def set(y: Int, x: Int, char: Char, fg: Int, bg: Int): Unit
-  def textk(y: Int, x: Int, text: String, fg: Int, bg: Int): Unit
+  def clear(): UIO[Unit]
+  def draw(): UIO[Unit]
+  def reset(): UIO[Unit]
+  def set(y: Int, x: Int, char: Char, fg: Int, bg: Int): UIO[Unit]
+  def textk(y: Int, x: Int, text: String, fg: Int, bg: Int): UIO[Unit]
+  def terminal: Terminal
+
+trait Action[T]:
+  val keys: List[String]
+  def convert: T
+
+def newTermGrid(height: Int, width: Int): Task[ITermGrid] = ???
+def inputLoop[T](actions: List[Action[T]], eventQueue: Queue[T], termGrid: ITermGrid): Task[Unit] =
+  ???
+def repl[T](actions: List[Action[T]], termGrid: ITermGrid)(logic: T => UIO[Unit]): UIO[Unit] = ???
