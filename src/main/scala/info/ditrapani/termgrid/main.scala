0ea2e85a917547097c241e53432c02398148c4e1
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-11 00:46:21 -0400

    Try out new TermGrid output.  Completely broken :(
    I'm realizing that StringBuilder.insert does not do what i thought.
    I thought it would overwrite the characters at that point.
    Like the way node buffers work.
    Unfortunately, it does what it says, inserts extra characters into the
    string, making the string longer.  Not what I need...
    
    I'll have to rethink this :(

--- src/main/scala/info/ditrapani/termgrid/main.scala
+++ src/main/scala/info/ditrapani/termgrid/main.scala
@@ -7,12 +7,18 @@ import zio.Console
 object Main extends zio.ZIOAppDefault:
   def run =
     for
-      termGrid <- newTermGrid(64, 40)
+      termGrid <- newTermGrid(10, 10)
+      _ <- termGrid.clear()
+      _ <- termGrid.draw()
       _ <- repl(termGrid) { key =>
-        Console.printLine(key).orDie.map { _ =>
+        for
+          _ <- termGrid.text(3, 5, key.toString, 5, 9)
+          _ <- termGrid.draw()
+        yield {
           key match
             case Alpha.q | Alpha.Q | Other.Esc => LoopControl.Stop
             case _ => LoopControl.Loop
         }
       }
+      _ <- termGrid.reset()
     yield (): Unit
